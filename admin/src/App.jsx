a784d5a144466819440095658ac4383fd4a2f617
Ashutosh maurya <ashutoshmauriyacse@gmail.com>
2025-10-14 20:13:07 +0530

    fix error in admin pannel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
-import Navbar from './components/NAvbar';
+import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard';
